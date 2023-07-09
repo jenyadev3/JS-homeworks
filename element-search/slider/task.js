9e41c0ef420b06602a70531e42f08011a8fd9e84
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-07-09 20:38:06 +0300

    внесены доработки

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -2,26 +2,50 @@ const prevButton = document.querySelector(".slider__arrow_prev");
 const nextButton = document.querySelector(".slider__arrow_next");
 const pics = document.querySelectorAll(".slider__item");
 const dots = document.querySelectorAll(".slider__dot");
-let currentIndex = 0;
 
-pics[currentIndex].classList.add("slider__item_active");
+function slider(position) {
+  let newPosition = position;
+  
+  // Проверяем, чтобы позиция не выходила за пределы слайдера
+  newPosition = newPosition < 0 ? pics.length - 1 : newPosition >= pics.length ? 0 : newPosition;
 
-prevButton.onclick = () => {
-  pics[currentIndex].classList.remove("slider__item_active");
-  currentIndex = (currentIndex - 1 + pics.length) % pics.length;
-  pics[currentIndex].classList.add("slider__item_active");
+  const activePic = pics[newPosition];
+  const activeDot = dots[newPosition];
+
+  activePic.classList.add("slider__item_active");
+  activeDot.classList.add("slider__dot_active");
+  
+  // Удаляем классы active у остальных слайдов и точек
+  pics.forEach((pic, index) => {
+    if (index !== newPosition) {
+      pic.classList.remove("slider__item_active");
+      dots[index].classList.remove("slider__dot_active");
+    }
+  });
 }
 
-nextButton.onclick = () => {
-  pics[currentIndex].classList.remove("slider__item_active");
-  currentIndex = (currentIndex + 1) % pics.length;
-  pics[currentIndex].classList.add("slider__item_active");
-}
+// Находим активный слайд
+const index = Array.from(pics).findIndex(pic =>
+  pic.classList.contains("slider__item_active"));
+
+slider(index);
+
+prevButton.addEventListener("click", () => {
+  const activeIndex = Array.from(pics).findIndex(pic =>
+    pic.classList.contains("slider__item_active"));
+  slider(activeIndex - 1);
+});
+
+nextButton.addEventListener("click", () => {
+  const activeIndex = Array.from(pics).findIndex(pic =>
+    pic.classList.contains("slider__item_active"));
+  slider(activeIndex + 1);
+});
 
 dots.forEach((dot, index) => {
-  dot.onclick = () => {
-    pics[currentIndex].classList.remove("slider__item_active");
-    currentIndex = index;
-    pics[currentIndex].classList.add("slider__item_active");
-  }
-})
+  dot.addEventListener("click", () => {
+    slider(index);
+  });
+});
+
+
