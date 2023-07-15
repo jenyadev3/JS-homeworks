e02ae3a7c713510236081280788f5089ed6824f5
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-07-15 21:59:58 +0300

    Update

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,9 +1,5 @@
 document.addEventListener("change", (event) => {
     const activeItem= event.target.closest(".interest")
     const childItems = activeItem.querySelectorAll(".interests_active .interest__check")
-    if (event.target.checked === true) {
-        childItems.forEach(item => item.checked = true)
-    } else {
-        childItems.forEach(item => item.checked = false)
-    }
+    return childItems.forEach(item => item.checked = event.target.checked ? true : false)
 })
