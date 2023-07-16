3d46b0c0569b6031c91a415f122254eb9c92b3f6
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-07-16 18:03:09 +0300

    Update

---
+++ document-structure/todo/task.js
@@ -0,0 +1,22 @@
+const list = document.querySelector(".tasks__list");
+const input = document.querySelector(".tasks__input");
+const addButton = document.querySelector(".tasks__add");
+
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const text = input.value;
+  const newTask = `<div class="task">
+  <div class="task__title">
+    ${text}
+  </div>
+  <a href="#" class="task__remove">&times;</a>
+  </div>`
+  list.insertAdjacentHTML("beforeend", newTask);
+  input.value = "";
+  list.addEventListener("click", (event) => {
+    if (event.target.classList.contains("task__remove")) {
+      event.target.closest(".task").remove(); 
+    }  
+  })
+})
+
