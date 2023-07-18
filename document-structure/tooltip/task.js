1ffb09a1a71d0f0da14ee42263e337163f9a3cc4
jenyadev3 <112981213+jenyadev3@users.noreply.github.com>
2023-07-18 19:54:20 +0300

    Добавлено закрытие подсказки по клику

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,36 @@
+let activeTooltip = null;
+
 const elements = document.querySelectorAll(".has-tooltip");
 
 elements.forEach((item) => {
   item.addEventListener("click", (event) => {
     event.preventDefault();
+
+    if (activeTooltip) {
+      activeTooltip.classList.remove("tooltip_active");
+      activeTooltip = null;
+      return;
+    }
+
     const text = item.getAttribute("title");
-    const tooltip = `<div class="tooltip">${text}</div>`;   
+    const tooltip = `<div class="tooltip">${text}</div>`;
     item.insertAdjacentHTML("afterend", tooltip);
     const tooltipElement = item.nextSibling;
-    tooltipElement.classList.toggle("tooltip_active");
+    tooltipElement.classList.add("tooltip_active");
     const position = item.getBoundingClientRect();
     tooltipElement.style.left = `${position.left}px`;
     tooltipElement.style.top = `${position.bottom}px`;
-  })
-})
+
+    activeTooltip = tooltipElement;
+  });
+});
+
+document.addEventListener("click", (event) => {
+  const target = event.target;
+  if (!target.classList.contains("has-tooltip")) {
+    if (activeTooltip) {
+      activeTooltip.classList.remove("tooltip_active");
+      activeTooltip = null;
+    }
+  }
+});
