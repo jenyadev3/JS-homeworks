f64255a060f49f67f5d14ce2cdd8bc6463a95ecd
Evgeniya <112981213+jenyadev3@users.noreply.github.com>
2023-07-23 17:23:45 +0300

    Update task.js

---
+++ client-state/auth/task.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("signin__btn");
+const form = document.getElementById("signin__form");
+const welcome = document.getElementById("welcome");
+const user = document.getElementById("user_id");
+
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  const loginData = {login: form.login.value, password: form.password.value};
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  xhr.onload = function() {
+    if(xhr.status >= 200 && xhr.status < 300) {
+      const response = JSON.parse(xhr.responseText);
+      if(response.success) {
+        const userId = response.user.id;
+        localStorage.setItem("id", userId)
+        user.textContent = userId;
+        welcome.classList.add("welcome_active");
+      } else {
+        alert("Неверный логин/пароль");
+      }
+    } else {
+       alert("Ошибка авторизации");
+  }
+};
+
+xhr.send(JSON.stringify(loginData));
+});
