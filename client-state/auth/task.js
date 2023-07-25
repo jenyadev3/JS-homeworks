c92c621cb774b4be0dbc8801d64a7d4dbfeefa8f
Evgeniya <112981213+jenyadev3@users.noreply.github.com>
2023-07-25 21:16:40 +0300

    Update task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -8,7 +8,7 @@ button.addEventListener("click", (event) => {
   const loginData = {login: form.login.value, password: form.password.value};
   const xhr = new XMLHttpRequest();
   xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/auth", true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("Content-Type", "application/json");
   
   xhr.onload = function() {
     if(xhr.status >= 200 && xhr.status < 300) {
